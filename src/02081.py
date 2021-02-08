d86677a017b750c513f8b49bede30043c286590a
penny56 <penny56@163.com>
2021-02-08 23:20:37 +0800

    NA

---
+++ src/02081.py
@@ -0,0 +1,38 @@
+#-- coding:utf8 --
+
+'''
+Created on Feb 08, 2021
+
+1. 学习leetcode算法
+
+@author: mayijie
+'''
+from eam_94335 import res
+from __builtin__ import True
+from Carbon.Aliases import true
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+        if x < 0 or (x >= 10 and x%10 == 0):
+            return False
+        elif x < 10:
+            return True
+        else:
+            later = 0
+            while True:
+                later = later*10+x%10
+                x = x/10
+                if later == x or later == x/10:
+                    return True
+                elif later > x:
+                    return False
+
+if __name__ == "__main__":
+    sol = Solution()
+    res = sol.isPalindrome(12233221)
+    print res

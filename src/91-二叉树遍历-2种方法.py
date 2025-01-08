1948b9a455b23350a852f8ae721c0e4a120b58f9
penny56 <penny56@163.com>
2025-01-08 14:10:31 +0800

    Add files via upload

--- src/91-二叉树遍历-2种方法.py
+++ src/91-二叉树遍历-2种方法.py
@@ -51,17 +51,29 @@ class Solution(object):
         return arr
 
     def midOrder(self, root, arr):
-        
-        if root == None: return
-        self.midOrder(root.left, arr)
-        arr.append(root.val)
-        self.midOrder(root.right, arr)
+
+        # if root == None: return
+        # self.midOrder(root.left, arr)
+        # arr.append(root.val)
+        # self.midOrder(root.right, arr)
 
 
-
-
-
-
+        # stack 中的元素是一个tuple： (<color>, <treeNode>)
+        # color 表示有没有访问过：
+        #        - white 表示没有访问，需要优先处理左右子树。
+        #        - gray 表示访问过了，直接写入或者显示吧。
+        (white, gray) = (0, 1)                          #1. 定义颜色
+        stack = [(white, root)]                         #2. 定义栈，并初始化
+        while stack:                                    #3. Loop
+            (color, curr) = stack.pop()                 #4. 出栈
+            if curr:                                    #5. 判断是否是一个节点？
+                # 是个非空节点，按颜色处理吧：
+                if color == white:                      # 注意这里要 append() 的是一个 tuple ，要双括号->
+                    stack.append((white, curr.right))   # 放栈底
+                    stack.append((gray, curr))          # 放中间，访问过啦。
+                    stack.append((white, curr.left))    # 放栈顶
+                else:
+                    arr.append(curr.val)
 
 if __name__ == "__main__":
     root = [1,2,3,4,5,None,8,None,None,6,7,9]

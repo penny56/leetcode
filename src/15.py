028e3ee9d718926aeadb880439d01a576280bf8d
penny56 <penny56@163.com>
2025-01-27 11:27:19 +0800

    threeSum

--- src/15.py
+++ src/15.py
@@ -1,50 +1,55 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        res = set()
-        for i, a in enumerate(nums):
-            for j, b in enumerate(nums):
-                if i != j and a + b == target:
-                    sorted_list = sorted([a, b])
-                    res.add(tuple(sorted_list))
-        return res
-        
+    '''
+    抄袭3指针解法
+    '''
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        resSet = set()
+        if len(nums) < 3: return []
+        s = set()
+        res = []
+        nums.sort()
+        for i, num in enumerate(nums):
+            left = i+1
+            right = len(nums)-1
+            while left < right:
+                if num+nums[left]+nums[right] < 0:
+                    left += 1
+                elif num+nums[left]+nums[right] == 0:
+                    if tuple([num, nums[left], nums[right]]) not in s:
+                        s.add(tuple([num, nums[left], nums[right]]))
+                        res.append([num, nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                else:
+                    right -= 1
+        return res
+
+    '''
+    对极端情况超时
+    '''
+    def threeSum2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        s = set()
+        res = []
         for i, a in enumerate(nums):
-            tmp = nums[:]
-            tmp.pop(i)
-            print (f"i = {i}, a = {a}")
-            twoSet = self.twoSum(tmp, -a)
-            if twoSet != None:
-                for two in twoSet:
-                    twoList = list(two)
-                    twoList.append(a)
-                    twoList.sort()
-                    resSet.add(tuple(twoList))
-                    print (f"twoSum: {tuple(twoList)}")
-        
-        resArray = []
-        for a in resSet:
-            resArray.append(list(a))
+            numsa = nums[:i]+nums[i+1:]
+            for j, b in enumerate(numsa):
+                numsab = numsa[:j]+numsa[j+1:]
+                if 0-(a+b) in numsab:
+                    temp = tuple(sorted([a, b, 0 - (a + b)]))
+                    if temp not in s:
+                        s.add(temp)
+                        res.append(list(temp))
 
-        return resArray
-
-
-# nums = [2,7,11,15]
-# target = 9
-nums = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
-
+        return res
 
 sol = Solution()
 
-res = sol.threeSum(nums)
-print ("res = ", res)
+
+print(sol.threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))

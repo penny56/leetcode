ed1d240c260166a2becb89525b18e25529c7a957
penny56 <penny56@163.com>
2026-04-14 11:37:31 +0800

    Create 1431.py

---
+++ src/1431.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandy = max(candies)
+        res = []
+
+        for candy in candies:
+            if candy + extraCandies >= maxCandy:
+                res.append(True)
+            else:
+                res.append(False)
+        
+        return res
+
